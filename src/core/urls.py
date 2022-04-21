51f0368c71185769a60e046b997bb4363fabfed0
Gustavo Henrique <gustavo.henrique@nappsolutions.com>
2022-04-20 22:18:49 -0300

    Add 404 and project templates

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('projects/<str:id>', views.projects),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def index(request):
@@ -8,3 +8,17 @@ def index(request):
         projects = json.load(file)
 
     return render(request, 'index.html', {'projects': projects})
+
+
+def projects(request, id):
+    with open('/programame/src/core/mock.json') as file:
+        projects = json.load(file)
+
+    filtered_projects = [p for p in projects if p['id'] == id]
+
+    if not filtered_projects:
+        return render(request, '404.html')
+
+    project = filtered_projects[0]
+
+    return render(request, 'project.html', {'project': project})
